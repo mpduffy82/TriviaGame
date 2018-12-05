8d66d047af0b2a0a20b770ae37ac2ce8fbf8ad31
Mykola Duffy <mpduffy82@gmail.com>
2018-12-04 22:30:24 -0500

    commit early, commit often

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,37 @@
 function displayStart() {
 	$("#stuffGoesHere").append('<a href="#" class="btn btn-danger btn-lg" id="start-button">' + "Engage!" + '</a>');
 };
+
+const myQuestions = [
+	{
+		//use the backtick ` instead of "" or ' for the question string
+	  question: `"Star Trek, The Next Generation" , takes us 99 years into the future after the original "Star Trek" television series finished. Who was in the captain's chair for this second series of the "Star Trek" franchise?`,
+	  answers: {
+		a: "Benjamin Sisko",
+		b: "Kathryn Janeway",
+		c: "Jonathan Archer",
+		d: "Jean-Luc Picard"
+	  },
+	  correctAnswer: "d"
+	},
+	{
+	  question: "What is the best site ever created?",
+	  answers: {
+		a: "SitePoint",
+		b: "Simple Steps Code",
+		c: "Trick question; they're both the best",
+		b: "Simple Steps Code"
+	  },
+	  correctAnswer: "c"
+	},
+	{
+	  question: "Where is Waldo really?",
+	  answers: {
+		a: "Antarctica",
+		b: "Exploring the Pacific Ocean",
+		c: "Sitting in a tree",
+		d: "Minding his own business, so stop asking"
+	  },
+	  correctAnswer: "d"
+	}
+  ];
